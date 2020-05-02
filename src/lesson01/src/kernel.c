301d3b0d371f4075d914b53edde6bc646c123944
Alan Francis <acf@alancfrancis.com>
2020-05-02 16:09:50 +0100

    just send over and over to avoid me worrying I started screen too late

--- src/lesson01/src/kernel.c
+++ src/lesson01/src/kernel.c
@@ -6,6 +6,7 @@ void kernel_main(void)
 	uart_send_string("Hello, world!\r\n");
 
 	while (1) {
-		uart_send(uart_recv());
+	//	uart_send(uart_recv());
+		uart_send_string("Hello, world!\r\n");
 	}
 }
